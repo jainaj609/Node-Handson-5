401f7e01455af19a5bd4858a12e6634df6b75caa
jainaj609 <jainaj609@gmail.com>
2023-05-26 21:35:59 +0530

    updated

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-const io = require('socket.io')({
-    cors: {
-      origin: '*',
-    },
-  });
-  
-  io.on('connection', (socket) => {
-    console.log('user connected',socket.id);
-  
-    socket.on('chat', (data) => {
-      console.log(`message received: ${data.message}`);
-      io.emit('message', {
-        username: data.username,
-        message: data.message,
-      });
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
+const socketio = require('socket.io')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("It's working")
+})
+const server = http.createServer(app);
+const io = socketio(server,{
+      cors: {
+        origin: '*',
+      },
+    })
+// const io = require('socket.io')({
+//     cors: {
+//       origin: '*',
+//     },
+//   });
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+
+  socket.on('chat', (data) => {
+    console.log(`message received: ${data.message}`);
+    io.emit('message', {
+      username: data.username,
+      message: data.message,
     });
-  
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
   });
-  
-  const port = 5000;
-  io.listen(port);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const port = 5000;
+server.listen(port,()=>{
   console.log(`Server listening on port ${port}`);
+});
